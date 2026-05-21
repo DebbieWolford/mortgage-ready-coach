c834f17bb25657d4b6e35692c188bf8d443cd2c6
DebbieWolford <dawolford@gmail.com>
2026-05-21 18:50:19 -0400

    Update progress.tsx

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -1,12 +1,7 @@
-'use client';
-import * as React from "react";
-import * as ProgressPrimitive from "@radix-ui/react-progress";
-import { cn } from "@/components/ui/utils";
-
-const Progress = React.forwardRef<React.ElementRef<typeof ProgressPrimitive.Root>, React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>>(({ className, value, ...props }, ref) => (
-  <ProgressPrimitive.Root ref={ref} className={cn("relative h-4 w-full overflow-hidden rounded-full bg-slate-200", className)} {...props}>
-    <ProgressPrimitive.Indicator className="h-full w-full flex-1 bg-slate-900 transition-all" style={{ transform: `translateX(-${100 - (value || 0)}%)` }} />
-  </ProgressPrimitive.Root>
-));
-Progress.displayName = ProgressPrimitive.Root.displayName;
-export { Progress };
+export function Progress({ value = 0, className = "", ...props }: any) {
+  return (
+    <div className={`w-full overflow-hidden rounded-full bg-slate-200 ${className}`} {...props}>
+      <div className="h-full bg-slate-900 transition-all" style={{ width: `${value}%` }} />
+    </div>
+  );
+}
